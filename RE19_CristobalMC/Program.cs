6331fa6046b96bebd2c36500b0b89f34df7011ca
cris4899 <59617766+cris4899@users.noreply.github.com>
2026-02-15 19:04:21 +0100

    Agregar archivos de proyecto.

---
+++ RE19_CristobalMC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RE19_CristobalMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
