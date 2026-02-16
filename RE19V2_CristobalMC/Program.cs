d9b5a6092e7693f76f172bd7ddb050ad4b499bd7
Castigao <59617766+cris4899@users.noreply.github.com>
2026-02-16 12:09:50 +0100

    a

---
+++ RE19V2_CristobalMC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RE19V2_CristobalMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
